aecebddb02eb0c4adbade71a234c695d345496b0
Francisco Garcia Molina <frangarmo93@gmail.com>
2019-10-16 17:54:18 +0200

    graficoDonas

---
+++ src/app/components/graficodona/graficodona.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-graficodona',
+  templateUrl: './graficodona.component.html',
+  styles: []
+})
+export class GraficodonaComponent implements OnInit {
+
+  @Input() leyenda: string  = '';
+  @Input('chartLabels') doughnutChartLabels: String[] = [];
+  @Input('chartData') doughnutChartData: number[] = [];
+  @Input('chartType') doughnutChartType: string  = '';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,40 @@
+import { NgModule } from '@angular/core';
+
+import { SharedModule } from '../shared/shared.module';
+import { PAGES_ROUTES } from './pages.routes';
+import { FormsModule } from '@angular/forms';
+
+// ng2-charts
+import { ChartsModule } from 'ng2-charts';
+
+import { PagesComponent } from './pages.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProgressComponent } from './progress/progress.component';
+import { Graficas1Component } from './graficas1/graficas1.component';
+
+// temporal
+import { IncrementadorComponent } from '../components/incrementador/incrementador.component';
+import { GraficodonaComponent } from '../components/graficodona/graficodona.component';
+
+@NgModule({
+  declarations: [
+    PagesComponent,
+    DashboardComponent,
+    Graficas1Component,
+    ProgressComponent,
+    IncrementadorComponent,
+    GraficodonaComponent
+  ],
+  exports: [
+    DashboardComponent,
+    Graficas1Component,
+    ProgressComponent
+  ],
+  imports: [
+    SharedModule,
+    PAGES_ROUTES,
+    FormsModule,
+    ChartsModule
+  ]
+})
+export class PagesModule {}
